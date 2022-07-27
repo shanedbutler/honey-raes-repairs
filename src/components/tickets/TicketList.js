9b9424ecff2c7c34543f48c72e032539e628ca37
minidiscs <shanedbutler@gmail.com>
2022-07-27 15:18:51 -0400

    add conditional for user content filtering

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react"
 import "./Tickets.css"
 
+const localHoneyUser = localStorage.getItem("honey_user")
+const honeyUserObject= JSON.parse(localHoneyUser)
+
 export const TicketList = () => {
     const [tickets, setTickets] = useState([])
+    const [filteredTickets, setFilteredTickets] = useState([])
 
     useEffect(
         () => {
@@ -14,11 +18,26 @@ export const TicketList = () => {
         [] // When this array is empty, you are observing initial component state
     )
 
+    useEffect(
+        () => {
+            if (honeyUserObject.staff) {
+                //for employees, see all tickets
+                setFilteredTickets(tickets)
+            }
+            else {
+                //for customers
+                const userTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFilteredTickets(userTickets)
+            }
+        },
+        [tickets]
+    )
+    
     return <>
         <h2>List of Tickets</h2>
         <article className="tickets">
             {
-                tickets.map((ticket) => {
+                filteredTickets.map((ticket) => {
                     return <section className="ticket">
                         <header>{ticket.description}</header>
                         <footer>Emergency: {ticket.emergency ? "Yes" : "No"}</footer>
