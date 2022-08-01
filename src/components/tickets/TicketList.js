9b7abad3479757d4e1b8bed00da9c216ad453978
minidiscs <shanedbutler@gmail.com>
2022-08-01 15:07:18 -0400

    search function added

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./Tickets.css"
 
-export const TicketList = () => {
+export const TicketList = ({ searchTermState }) => {
 
     const localHoneyUser = localStorage.getItem("honey_user")
     const honeyUserObject= JSON.parse(localHoneyUser)
@@ -13,6 +13,11 @@ export const TicketList = () => {
     const [filteredTickets, setFilteredTickets] = useState([])
     const [emergency, setEmergency] = useState(false)
     const [completed, setCompleted] = useState(false)
+
+    useEffect(() => { //filter tickets by search term passed in via props
+        const searchedTickets = tickets.filter(ticket => ticket.description.toLowerCase().startsWith(searchTermState.toLowerCase()))
+        setFilteredTickets(searchedTickets)
+    }, [searchTermState])
 
     useEffect( //filter tickets by emergencies
         () => {
