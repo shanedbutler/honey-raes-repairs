e02253b755fd360a47cad9424c6488dd321f7a28
minidiscs <shanedbutler@gmail.com>
2022-08-01 13:40:36 -0400

    variables outside of function bugfix

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./Tickets.css"
 
-const localHoneyUser = localStorage.getItem("honey_user")
-const honeyUserObject= JSON.parse(localHoneyUser)
-
 export const TicketList = () => {
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject= JSON.parse(localHoneyUser)
 
     const navigate = useNavigate()
 
